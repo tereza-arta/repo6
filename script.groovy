a98eadf2bd2824ea1af2a682c3fc578c6a1adea9
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-03-08 22:15:49 +0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,9 @@
 void function() {
-  writeFile file: 'groovy1.txt', text: env.TARGET_BRANCH
-  sh 'ls -l groovy1.txt'
-  sh 'cat groovy1.txt'
-  final String line = env.TARGET_BRANCH
+  writeFile file: 'groovy.txt', text: env.TARGET_BRANCH
+  sh 'ls -l groovy.txt'
+  sh 'cat groovy.txt'
+  def line = env.TARGET_BRANCH
   echo line
-  def text = 'Just saying: "Groovy is gr8!"'
-  assert text.takeBetween('is'.toString(), '!'.toString()) == " gr8"
 }
 
 return this
